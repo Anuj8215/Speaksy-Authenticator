d3311b6d7f2c27fb94ec33574c92bc0a87d89e79
Anuj <anujpawar1010@gmail.com>
2025-09-04 13:37:14 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Speaksy Auth</h1>
+      <p>React frontend is running 🎉</p>
+    </div>
+  );
+}
